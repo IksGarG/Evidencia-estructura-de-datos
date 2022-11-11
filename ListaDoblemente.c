5beb000042761fff1d7acde2d93986fc298187e5
Iker Garcia <A01782767@tec.mx>
2022-11-10 18:42:31 -0600

    Comentado

--- ListaDoblemente.c
+++ ListaDoblemente.c
@@ -7,9 +7,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-// I need a function to insert at the beginning of the list
 void beg_insert(int);
+
+/* Esta es una definición de estructura en C para un nodo de una lista doblemente enlazada. 
+    El nodo tiene un campo de datos entero y dos punteros a otros nodos. 
+    El puntero next apunta al siguiente nodo de la lista, y el puntero prev apunta al nodo anterior de la lista. 
+    El puntero head apunta al primer nodo de la lista. */
 
 struct node
 {
@@ -18,6 +21,15 @@ struct node
     struct node *prev;
 };
 struct node *head;
+
+/* Esta es una función para insertar un nuevo nodo al principio de una lista doblemente enlazada. 
+    La función toma un parámetro entero, que es el dato a almacenar en el nuevo nodo. 
+    La función primero asigna memoria para el nuevo nodo usando malloc, y luego comprueba si la asignación fue exitosa. 
+    Si no es así, imprime un mensaje de error y regresa. En caso contrario, almacena los datos en el nuevo nodo, y luego comprueba si la lista está vacía. 
+    Si es así, establece el puntero de la cabeza para que apunte al nuevo nodo y establece los punteros next y prev del nuevo nodo para que apunten a sí mismo.
+    Si la lista no está vacía, la función encuentra el último nodo de la lista y establece su puntero siguiente para que apunte al nuevo nodo. 
+    También establece el puntero prev del nuevo nodo para que apunte al último nodo, y establece el puntero prev del nodo principal para que apunte al nuevo nodo. 
+    Por último, establece el puntero de la cabeza para que apunte al nuevo nodo. */
 
 void beg_insert(int item)
 {
@@ -52,6 +64,7 @@ void beg_insert(int item)
         printf("Node Insertado\n\n");
     }
 }
+// La función está iterando a través de una lista enlazada e imprimiendo los datos de cada nodo.
 
 void display()
 {
@@ -65,8 +78,7 @@ void display()
     }
     printf("\n%d", ptr->data);
 }
-
-// Function to insert multiple nodes at the beginning of the list
+// La toma un número entero de entrada n e inserta n elementos en una lista enlazada, empezando por el principio de la lista.
 
 void insert_multiple(int n)
 {
@@ -79,7 +91,8 @@ void insert_multiple(int n)
     }
 }
 
-// Function to delete a node
+/*  La función borra un nodo de una lista enlazada. Si el nodo a eliminar es el primer nodo, entonces la función establece que el siguiente nodo sea la cabeza. 
+    En caso contrario, la función establece que el siguiente nodo del nodo anterior sea el siguiente nodo del nodo a eliminar. */
 
 void delete_node(int item)
 {
@@ -124,8 +137,8 @@ void delete_node(int item)
         printf("Objeto no encontrado\n\n");
     }
 }
-
-// Function to delete multiple nodes
+// La funcion borra múltiples nodos de una lista enlazada. 
+// Se pide al usuario que introduzca el elemento a eliminar, y se llama a la función delete_node para cada elemento.
 
 void delete_multiple(int n)
 {
@@ -137,8 +150,8 @@ void delete_multiple(int n)
         delete_node(item);
     }
 }
-
-// Function to delete the entire list
+// La función borra una lista circular enlazada. El bucle while itera a través de toda la lista, borrando cada nodo uno a uno hasta llegar de nuevo al nodo cabeza. 
+// El nodo cabeza es entonces borrado y el puntero cabeza es puesto a NULL.
 
 void delete_list()
 {
@@ -154,9 +167,8 @@ void delete_list()
     head = NULL;
     printf("Lista eliminada\n\n");
 }
-
-// Function to search for a node
-
+// Esta función busca un nodo en una lista enlazada. Si se encuentra el elemento, imprime la posición del mismo. 
+// Si no se encuentra el elemento, imprime "Objeto no encontrado".
 void search_node(int item)
 {
     struct node *ptr;
@@ -168,7 +180,7 @@ void search_node(int item)
         {
             if (ptr->data == item)
             {
-                printf("\n\nObjeto encontrado en la posicion %d", i + 1);
+                printf("\n\nObjeto encontrado en la posicion %d\n\n", i + 1);
                 flag = 1;
             }
             i++;
@@ -184,9 +196,7 @@ void search_node(int item)
         printf("Lista vacia");
     }
 }
-
-// Function to know if the list is empty
-
+// Esta función indica al usuario si la lista se encuentra vacía o no. Apuntando el puntero head a NULL, se indica que la lista está vacía.
 void is_empty()
 {
     if (head == NULL)
@@ -198,9 +208,7 @@ void is_empty()
         printf("Lista no vacia");
     }
 }
-
-// Function to count the number of nodes
-
+// La función cuenta el número de nodos de una lista. Si la lista está vacía, imprime "Lista vacía".
 void count_nodes()
 {
     struct node *ptr;
@@ -221,8 +229,7 @@ void count_nodes()
     }
 }
 
-// Function to display the list in reverse order
-
+// Esta función muestra los nodos de la lista enlazada en orden inverso.
 void display_reverse()
 {
     struct node *ptr;
@@ -235,9 +242,7 @@ void display_reverse()
     }
     printf("\n%d", ptr->data);
 }
-
-// Function to delete duplicate nodes
-
+// Esta función elimina los valores de los nodos duplicados en una lista enlazada circular.
 void delete_duplicate()
 {
     struct node *ptr1, *ptr2, *dup;
@@ -262,9 +267,8 @@ void delete_duplicate()
         ptr1 = ptr1->next;
     }
 }
-
-// Main function with the menu
-
+// Se trata de un programa basado en menús. El usuario puede elegir entre las diferentes opciones que se le muestran en pantalla.
+// El bucle do-while se utiliza para que el menú se muestre al menos una vez aunque la condición del bucle while falle.
 int main()
 {
     int choice, item, n;
