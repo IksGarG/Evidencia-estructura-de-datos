e8df0c8cf9cf30e459b984e5b23a6032d60c687d
ronuma <ronum.00@gmail.com>
2022-12-01 17:31:04 -0600

    update

--- 8reinas/brute_force.py
+++ 8reinas/brute_force.py
@@ -17,14 +17,7 @@ def print_board():
 # insertar una reina en el tablero, no evalua si es posible insertarla porque eso lo hace el main
 def insert_queen(row, col):
         for i in range(8):
-                if board[row][i] == "0":
-                        board[row][i] = "X"
-                if board[i][col] == "0":
-                        board[i][col] = "X"
-                for j in range(8):
-                        if (i+j) == (row+col) or (i-j) == (row-col):
-                                board[i][j] = "X"
-        board[row][col] = "Q"
+                board[row][col] = "Q"
         print("Reina colocada en la posicion: ", row, ",", col)
         print_board()
 
@@ -71,44 +64,30 @@ def main():
                                 for col1 in range(8):
                                         if not isThreatened(row1, col1):
                                                 insert_queen(row1, col1)
-                                        else:
-                                            remove_queen(row1, col1)
                                         for row2 in range(8):
                                                 for col2 in range(8):
                                                         if not isThreatened(row2, col2):
                                                                 insert_queen(row2, col2)
-                                                        else:
-                                                                remove_queen(row2, col2)
                                                         for row3 in range(8):
                                                                 for col3 in range(8):
                                                                         if not isThreatened(row3, col3):
                                                                                 insert_queen(row3, col3)
-                                                                        else:
-                                                                                remove_queen(row3, col3)
                                                                         for row4 in range(8):
                                                                                 for col4 in range(8):
                                                                                         if not isThreatened(row4, col4):
                                                                                                 insert_queen(row4, col4)
-                                                                                        else:
-                                                                                                remove_queen(row4, col4)
                                                                                         for row5 in range(8):
                                                                                                 for col5 in range(8):
                                                                                                         if not isThreatened(row5, col5):
                                                                                                                 insert_queen(row5, col5)
-                                                                                                        else:
-                                                                                                                remove_queen(row5, col5)
                                                                                                         for row6 in range(8):
                                                                                                                 for col6 in range(8):
                                                                                                                         if not isThreatened(row6, col6):
                                                                                                                                 insert_queen(row6, col6)
-                                                                                                                        else:
-                                                                                                                                remove_queen(row6, col6)
                                                                                                                         for row7 in range(8):
                                                                                                                                 for col7 in range(8):
                                                                                                                                         if not isThreatened(row7, col7):
                                                                                                                                                 insert_queen(row7, col7)
                                                                                                                                                 print("Solucion encontrada!")
-                                                                                                                                                print_board()
-                                                                                                                                        else:
-                                                                                                                                                remove_queen(row7, col7)                                                                                                                                                             
+                                                                                                                                                print_board()                                                                                                                                                        
 main()
