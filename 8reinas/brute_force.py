1f55c221a9f20b1475e2aa7388bf352ba519a622
ronuma <ronum.00@gmail.com>
2022-12-01 19:30:55 -0600

    logrado brute force

--- 8reinas/brute_force.py
+++ 8reinas/brute_force.py
@@ -1,53 +1,55 @@
 board = [
-        ["0","0","0","0","0","0","0","0"],
-        ["0","0","0","0","0","0","0","0"],
-        ["0","0","0","0","0","0","0","0"],
-        ["0","0","0","0","0","0","0","0"],
-        ["0","0","0","0","0","0","0","0"],
-        ["0","0","0","0","0","0","0","0"],
-        ["0","0","0","0","0","0","0","0"],
-        ["0","0","0","0","0","0","0","0"]
+        ["·","·","·","·","·","·","·","·"],
+        ["·","·","·","·","·","·","·","·"],
+        ["·","·","·","·","·","·","·","·"],
+        ["·","·","·","·","·","·","·","·"],
+        ["·","·","·","·","·","·","·","·"],
+        ["·","·","·","·","·","·","·","·"],
+        ["·","·","·","·","·","·","·","·"],
+        ["·","·","·","·","·","·","·","·"],
         ]
 
 # imprimir el tablero
 def print_board():
     for row in board:
         print(row)
+    print("")
 
 # evaluar si una posicion esta amenazada por otra reina, para fines practicos
-def isThreatened(row, col):
+def conflict(row, col):
         for i in range(8):
-                if board[row][i] == "Q" or board[i][col] == "Q":
+                if board[i][col] == "Q" and i != row:
                         return True
                 for j in range(8):
-                        if (i+j) == (row+col) or (i-j) == (row-col):
+                        if ((i+j) == (row+col) or (i-j) == (row-col)) and (i != row and j != col):
                                 if board[i][j] == "Q":
                                         return True
         return False
 
-def check(row, col):
-        if (isThreatened(row, col)):
-                board[row][col] = "0"
-        else:
-                board[row][col] = "Q"
-                                
+def insert(row, col):
+        board[row][col] = "Q"
+        board[row][col-1] = "·"
+
 def main():
         for col0 in range(8):
-                check(0, col0)
+                insert(0, col0)
                 for col1 in range(8):
-                        check(1, col1)
+                        insert(1, col1)
                         for col2 in range(8):
-                                check(2, col2)
+                                insert(2, col2)
                                 for col3 in range(8):
-                                        check(3, col3)
+                                        insert(3, col3)
                                         for col4 in range(8):
-                                                check(4, col4)
+                                                insert(4, col4)
                                                 for col5 in range(8):
-                                                        check(5, col5)
+                                                        insert(5, col5)
                                                         for col6 in range(8):
-                                                                check(6, col6)
+                                                                insert(6, col6)
                                                                 for col7 in range(8):
-                                                                        check(7, col7)
-        print_board()
+                                                                        insert(7, col7)
+                                                                        if not conflict(0, col0) and not conflict(1, col1) and not conflict(2, col2) and not conflict(3, col3) and not conflict(4, col4) and not conflict(5, col5) and not conflict(6, col6) and not conflict(7, col7):
+                                                                                print("found!")
+                                                                                print_board()
+                                                                                quit()
 
 main()
--- 8reinas/dynamic.py
+++ 8reinas/dynamic.py
@@ -1,17 +1,17 @@
 N = 8
 # NxN matrix with all elements set to 0
-board = [[0]*N for _ in range(N)]
+board = [["·"]*N for _ in range(N)]
 
 def attack(i, j):
     #checking vertically and horizontally
     for k in range(N):
-        if board[i][k]==1 or board[k][j] == 1:
+        if board[i][k]=="Q" or board[k][j] == "Q":
             return True
     #checking diagonally
     for k in range(N):
         for l in range(N):
             if (k+l==i+j) or (k-l == i-j):
-                if board[k][l] == 1:
+                if board[k][l] == "Q":
                     return True
     return False
 
@@ -20,11 +20,11 @@ def N_queens(n):
         return True
     for i in range(N):
         for j in range(N):
-            if (not(attack(i,j))) and (board[i][j] != 1):
-                board[i][j] = 1
+            if (not(attack(i,j))) and (board[i][j] != "Q"):
+                board[i][j] = "Q"
                 if N_queens(n-1) == True:
                     return True
-                board[i][j] = 0
+                board[i][j] = "·"
     return False
     
 N_queens(N)
